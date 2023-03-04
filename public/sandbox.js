6aebf6aa0da4c9c7fc32622cfa0d3a316eda81cb
cmejia49 <mejiachristopher49@gmail.com>
2023-03-04 22:15:59 +0800

    data types typescript

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,37 @@
 "use strict";
-console.log("test");
+//let decimal: number = 6;
+let hex = 0xf00d;
+let binary = 0b1010;
+let octal = 0o744;
+//boolean
+let isDone = false;
+//string
+let color = "blue";
+color = 'red';
+let fullName = `Bob Bobbington`;
+let age = 37;
+let sentence = `Hello, my name is ${fullName}.
+ 
+I'll be ${age + 1} years old next month.`;
+console.log(sentence);
+//Array
+let list = [1, 2, 3];
+let listGeneric = [1, 2, 3];
+//tuple
+let x; //declare
+// Initialize it
+x = ["hello", 10]; // OK
+console.log(x[0].substring(1));
+//Enum
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+})(Color || (Color = {}));
+let c = Color.Green;
+//Unknown
+let notSure = 4;
+notSure = "maybe a string instead";
+// OK, definitely a boolean
+notSure = false;
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,44 @@
-console.log("test");
+//let decimal: number = 6;
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+//boolean
+let isDone: boolean = false;
+
+//string
+let color: string = "blue";
+color = 'red';
+
+let fullName: string = `Bob Bobbington`;
+let age: number = 37;
+let sentence: string = `Hello, my name is ${fullName}.
+ 
+I'll be ${age + 1} years old next month.`;
+console.log(sentence);
+
+//Array
+let list: number[] = [1, 2, 3];
+
+let listGeneric: Array<number> = [1, 2, 3];
+
+//tuple
+let x: [string, number];//declare
+// Initialize it
+x = ["hello", 10]; // OK
+console.log(x[0].substring(1));
+
+//Enum
+enum Color {
+    Red,
+    Green,
+    Blue,
+  }
+  let c: Color = Color.Green;
+
+  //Unknown
+let notSure: unknown = 4;
+notSure = "maybe a string instead";
+ 
+// OK, definitely a boolean
+notSure = false;
