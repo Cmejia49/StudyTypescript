181eb28b3d16c0ff161ecae8af3079ab98965b35
cmejia49 <mejiachristopher49@gmail.com>
2023-03-05 20:56:16 +0800

    basic dom manipulation

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("test");
+/**
+ *
+ * Based on Tomasz Nurkiewiczs answer, the "problem" is that typescript is typesafe. :)
+ * So the document.getElementById() returns the type HTMLElement which does not contain a value property.
+ * The subtype HTMLInputElement does however contain the value property.
+ *
+ */
+let selectedItem = () => {
+    var item = document.getElementById("type");
+    console.log(item.value);
+};
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,12 @@
-console.log("test");
+
+/**
+ * 
+ * Based on Tomasz Nurkiewiczs answer, the "problem" is that typescript is typesafe. :) 
+ * So the document.getElementById() returns the type HTMLElement which does not contain a value property. 
+ * The subtype HTMLInputElement does however contain the value property.
+ * 
+ */
+let selectedItem = (): void => {
+    var item = <HTMLInputElement>document.getElementById("type");
+    console.log(item.value);
+}
