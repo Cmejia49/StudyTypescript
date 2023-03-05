82c772e5e114c8dbc7c245a0c2c77305e534546e
cmejia49 <mejiachristopher49@gmail.com>
2023-03-05 19:54:56 +0800

    interface and class typescript

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("test");
+class developer {
+    constructor(name, age) {
+        this.age = age;
+        this.name = name;
+    }
+    getNameAndAge() {
+        console.log(`developer name is  ${dev.name} age of ${dev.age}`);
+    }
+}
+const dev = new developer("chrisss", 132);
+dev.getNameAndAge();
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,1 +1,22 @@
-console.log("test");
+interface Ideveloper{
+
+    getNameAndAge():void;
+}
+
+
+class developer implements Ideveloper {
+    
+    name: string;
+    age: number;
+    constructor(name:string,age:number) {
+        this.age = age;
+        this.name = name;
+
+    }
+    getNameAndAge(): void {
+        console.log(`developer name is  ${dev.name} age of ${dev.age}`);
+    }
+}
+
+const dev = new developer("chrisss",132);
+dev.getNameAndAge();
